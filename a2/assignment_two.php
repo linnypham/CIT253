34af85f14c5803a92198021aad60b08fb0aa5a19
Bishie <40342277+Bishie@users.noreply.github.com>
2022-01-29 12:32:04 -0500

    a2

---
+++ a2/assignment_two.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD/XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml11-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <title>Variables and Arithmetic Operations</title>
+</head>
+<body>
+
+<?php 
+  $number_one = "25";
+  $number_two = "32";
+  $addition = $number_one + $number_two;
+  $difference = $number_two - $number_one;
+  $product = $number_one * $number_two;
+  $division = $number_two / $number_one;
+  $remainder = $number_two % $number_one;
+
+  echo $number_one." + ".$number_two." = ".$addition."<br/>";
+  echo $number_two." - ".$number_one." = ".$difference."<br/>";
+  echo $number_one." * ".$number_two." = ".$product."<br/>";
+  echo $number_two." / ".$number_one." = ".$division."<br/>";
+  echo $number_two." % ".$number_one." = ".$remainder."<br/>";
+?>
+
+</body>
+</html>
